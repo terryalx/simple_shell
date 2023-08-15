b9c2b6323a097bfe3ed681aea49becfee5e9782a
setalxswe <setalxswe@gmail.com>
2023-08-15 01:56:34 -0700

    more functions

---
+++ print_list_env.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "shell.h"
+#include "list.h"
+
+/**
+ * print_list_env - prins environment variables
+ * @head: pointer -> head of the environment list
+*/
+
+void print_list_env(list_t *head)
+{
+	if (head)
+	{
+		print_list_env(head->next);
+		if (head->str)
+			_printf("%s=%s\n", head->str, head->val);
+	}
+}
---
+++ printenv.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "shell.h"
+#include "list.h"
+
+/**
+ * _printenv - prints environment
+ * @params: parameters
+ * Return: void
+ */
+
+void _printenv(param_t *params)
+{
+	if (params->tokCount != 1)
+	{
+		_printf("env: %s: No such file or directory\n",
+			params->args[1]);
+		params->status = 2;
+		return;
+	}
+	print_list_env(params->env_head);
+	params->status = 0;
+}
