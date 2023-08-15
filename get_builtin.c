592e9cbc388cee971d5cbbeabcac641d3028c313
setalxswe <setalxswe@gmail.com>
2023-08-15 00:55:15 -0700

    builtin

---
+++ get_builtin.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "shell.h"
+#include "lists.h"
+#include "main.h"
+
+/**
+ * get_builtin - All built-in function
+ * @params: Pointer to param_t
+ * Return: Built-in function pionter. NULL if not found.
+ */
+
+void (*get_builtin(param_t *params))(param_t *)
+{
+	op_t ops[] = {
+		{"exit", _myExit},
+		{"clear", _clear},
+		{"env", _printenv},
+		{"setenv", _setenv},
+		{"cd", _cd},
+		{"unsetenv", _unsetenv},
+		{"alias", _alias},
+		{NULL, NULL},
+	};
+	op_t *op = ops;
+
+	while (op->name)
+	{
+		if (!_strcmp(params->args[0], op->name))
+			return (op->func);
+		op++;
+	}
+	return (NULL);
+}
