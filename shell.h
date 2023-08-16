9b9e59a2066b31168deb7aca6bc3062b04954092
setalxswe <setalxswe@gmail.com>
2023-08-16 14:04:32 -0700

    add_node.c alias.c _atoi.c cd.c clear.c flush_buffer.c free_list.c free_params.c get-arg.c get_binary.c get_builtin.c get_char.c getenv.c get_file.c getline.c get_node.c get_number.c get_rev.c get_rot13.c get_string.c get_type.c hsh init_param.c isDelim.c list.h list_len.c main.c main.h make_alias.c move_left.c myExit.c print_alias.c print_all_aliases.c print_arg.c printenv.c _printf.c print_list_alias.c print_list.c print_list_env.c process_string.c README.md _realloc.c rev_string.c rlLine.c rot13.c run_command.c set_alias.c _setenv.c shell.h sigint_handler.c _strchr.c _strcmp.c _strcmp_n.c str_concat.c _strcpy.c _strdup.c string_nconcat.c _strlen.c _strtok.c _unsetenv.c validNum.c write_error.c

--- shell.h
+++ shell.h
@@ -1,59 +1,106 @@
-#ifndef _SHELL_H
-#define _SHELL_H
-#define _GNU_SOURCE
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
-#include "list.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <signal.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
+#define BUFFER_SIZE 4096
+#define READ_SIZE 1024
+
+/**
+ * Data structure to hold information about environment variables.
+ */
+typedef struct list_s
+{
+    char *str;                  /* The name of the environment variable */
+    char *val;                  /* The value of the environment variable */
+    unsigned int val_len;       /* Length of the value */
+    struct list_s *next;        /* Pointer to the next node in the list */
+} list_t;
+
+/**
+ * Data structure to hold shell parameters.
+ */
 typedef struct param_s
 {
-	char **argv;
-	char *buffer;
-	char **args;
-	char *nextCommand;
-	unsigned int argsCap;
-	unsigned int lineCount;
-	unsigned int tokCount;
-	int status;
-	list_t *env_head;
-	list_t *alias_head;
+    unsigned int argsCap;       /* Capacity of args array */
+    unsigned int lineCount;     /* Line count */
+    unsigned int tokCount;      /* Token count */
+    int status;                 /* Exit status */
+    char **argv;                /* Command-line arguments */
+    char *nextCommand;          /* Next command string */
+    char *buffer;               /* Input buffer */
+    char **args;                /* Argument list */
+    list_t *env_head;           /* Environment variables list */
+    list_t *alias_head;         /* Alias list */
 } param_t;
 
-typedef struct op
-{
-	char *name;
-	void (*func)(param_t *);
-} op_t;
+/* Function prototypes */
 
-int process_string(param_t *);
-char *_strdup(char *);
-char *_strcpy(char *dest, const char *src);
-int _getline(param_t *);
-int _strcmp(char *, char *);
-int _strcmp_n(char *, char *, int n);
-char *_strtok(char *str, char *delim, char **savePtr);
-void run_command(param_t *);
-char **_realloc(char **ptr, unsigned int old_size, unsigned int new_size); /*Here*/
-void _printenv(param_t *);
-void sigint_handler(int);
-char *_getenv(char *name, param_t *params);
-void _setenv(param_t *params);
-void _unsetenv(param_t *params);
-char *get_file(param_t *params);
-void (*get_builtin(param_t *params))(param_t *);
-void _myExit(param_t *params);
-void _cd(param_t *params);
-void _alias(param_t *params);
-void set_alias(char *name, param_t *params);
+/* main.c */
+int main(int __attribute__((unused)) argc, char **argv, char **env);
+
+/* list.c */
+list_t *add_node(list_t **head, char *str, char *val);
+void free_list(list_t *head);
+list_t *get_node(list_t *head, char *str);
+size_t print_list(list_t *h);
+size_t list_len(list_t *h);
+
+/* shell.c */
+void run_command(param_t *params);
+void sigint_handler(int __attribute__((unused)) s);
+
+/* helpers.c */
+int _putchar(char c);
+int _puts(char *s);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+char *_strdup(char *str);
+char *_strtok(char *str, char *delim, char **saveptr);
+int _strcmp(char *s1, char *s2);
+int _strcmp_n(char *s1, char *s2, size_t n);
+int _atoi(char *s);
+char *get_number(unsigned int n);
+void free_params(param_t *params);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* string_functions.c */
+char *get_string(char *s);
+char *get_rev(char *s);
+char *get_rot13(char *s);
+char *str_concat(char *s1, char *s2);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *move_left(char *a, int size, int num);
+void rev_string(char *s);
+char *rot13(char *s);
+
+/* list_helpers.c */
+void print_list_env(list_t *head);
+void print_list_alias(list_t *head);
+void print_all_aliases(param_t *params);
+void print_arg(char *arg);
+void _printenv(param_t *params);
 void make_alias(char *name, char *val, param_t *params);
 void print_alias(char *name, param_t *params);
-void print_all_aliases(param_t *params);
-void print_list_env(list_t *);
-void print_list_alias(list_t *);
-void free_params(param_t *params);
-void write_error(param_t *params, char *msg);
-void _clear(param_t *params);
 
+/* validation.c */
+int validNum(char *s);
+void write_error(param_t *params, char *msg);
+
+/* getline.c */
+int _getline(param_t *params);
+char *rlLine(char **line, unsigned int oldSize, unsigned int newSize);
+
+/* init_param.c */
 param_t *init_param(char **argv, char **env);
 
-#endif /*end _SHELL_H*/
+#endif /* SHELL_H */
