d271a7e962a512bc9f4edc1e7ba6c177a9d503a3
setalxswe <setalxswe@gmail.com>
2023-08-19 01:12:59 -0700

    add_node.c alias.c _atoi.c cd.c clear_screen.c create_setenv_args.c create_tmp_args.c flush_buffer.c free_list.c free_params.c free_tmp_args.c get-arg.c get_binary.c get_builtin.c get_char.c getenv.c get_file.c getline.c get_node.c get_number.c get_rev.c get_rot13.c get_string.c get_type.c handle_chdir_error.c handle_dash.c handle_malloc_error.c hsh init_param.c isDelim.c list_len.c main.c make_alias.c move_left.c myExit.c print_alias.c print_all_aliases.c print_arg.c printenv.c _printf.c print_list_alias.c print_list.c print_list_env.c process_string.c README.md _realloc.c restore_original_args.c rev_string.c rlLine.c rot13.c run_command.c set_alias.c _setenv.c setenv_oldpwd.c setenv_pwd.c shell.h sigint_handler.c _strchr.c _strcmp.c _strcmp_n.c str_concat.c _strcpy.c _strdup.c string_nconcat.c _strlen.c token_.c _unsetenv.c update_oldpwd.c validNum.c write_error.c

--- create_setenv_args.c
+++ create_setenv_args.c
@@ -2,31 +2,45 @@
 #include <stdlib.h>
 #include <limits.h>
 
-/*
+/**
+ * create_setenv_args - Create an array of arguments for the setenv command
+ * @command: The command name
+ * @variable: The variable name
+ * @value: The value to set the variable to
  *
- *
- *
- *
- *
- *
- * */
-
+ * Return: The array of arguments for the setenv command
+ */
 char **create_setenv_args(char *command, char *variable, char *value)
 {
-	char **setenvArgs = malloc(sizeof(char *) * 4);
-	if (!setenvArgs)
-		return NULL;
+	char **args = malloc(sizeof(char *) * 4);
+	if (!args)
+		return (NULL);
 
-	setenvArgs[0] = _strdup(command);
-	setenvArgs[1] = _strdup(variable);
-	setenvArgs[2] = _strdup(value);
-	setenvArgs[3] = NULL;
-
-	if (!setenvArgs[0] || !setenvArgs[1] || !setenvArgs[2])
+	args[0] = _strdup(command);
+	if (!args[0])
 	{
-		free_tmp_args(setenvArgs);
+		free(args);
+		return (NULL);
+	}
+
+	args[1] = _strdup(variable);
+	if (!args[1])
+	{
+		free(args[0]);
+		free(args);
+		return (NULL);
+	}
+
+	args[2] = _strdup(value);
+	if (!args[2])
+	{
+		free(args[1]);
+		free(args[0]);
+		free(args);
 		return NULL;
 	}
 
-	return setenvArgs;
+	args[3] = NULL;
+
+	return (args);
 }
