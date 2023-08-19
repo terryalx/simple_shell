64b48e43edea8276dc10dd1f31e12a635419489e
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-19 16:56:50 +0100

    alt-fix

--- _strchr.c
+++ _strchr.c
@@ -10,17 +10,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
+	int i;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
 	if (c == '\0')
-	{
-		return (s);
-	}
-
-	return (NULL);
+		return (&s[i]);
+	return (0);
 }
--- _strcmp_n.c
+++ _strcmp_n.c
@@ -5,32 +5,30 @@
 
 /**
  * _strcmp_n - compares n bytes of two strings
- * @str1: the first string to compare
- * @str2: the second string to compare
- * @num: the number of bytes to compare
+ * @s1: the first string to compare
+ * @s2: the second string to compare
  *
  * Return: an integer less than, equal to, or greater than 0 if str1 is,
- * respectively, less than, equal to, or greater than str2
+ * respectively, less than, equal to, or greater than s2
  *
  * Description: This function compares the first `num` bytes of the strings
- * `str1` and `str2`. It returns an integer value that indicates whether `str1`
- * is less than, equal to, or greater than `str2`. The comparison is performed
+ * `s1` and `s2`. It returns an integer value that indicates whether `1`
+ * is less than, equal to, or greater than `s2`. The comparison is performed
  * by subtracting the corresponding characters of the strings. If the result is
  * 0, the characters are equal; if the result is positive, `str1` is greater;
- * if the result is negative, `str1` is smaller.
+ * if the result is negative, `s1` is smaller.
  */
-int _strcmp_n(char *str1, char *str2, int num)
+int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int res = *str1 - *str2;
+	int res = 0;
 
-	while (i <= num)
-	{
-		res = *(str1++) - *(str2++);
-		if (res != 0)
+	do {
+		res = *s1 - *s2;
+		if (*s1 == 0 || *s2 == 0)
 			break;
-		i++;
-	}
+		s1++;
+		s2++;
+	} while (res == 0);
 
 	return (res);
 }
