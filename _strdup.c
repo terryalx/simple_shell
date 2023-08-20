2dcd56dce5ff44b91a99cf96366341d493e4b99e
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-20 11:38:25 +0100

    fix

--- _strdup.c
+++ _strdup.c
@@ -1,35 +1,39 @@
 #include "shell.h"
 
 /**
- * _strdup - copy a string to a new allocated block of memory
- * @str: string
- * Return: pointer to duplicated string, NULL if insufficient memory
+ * _strdup - Duplicate a string to a new allocated block of memory
+ * @str: String to duplicate
+ *
+ * Return: Pointer to duplicated string, NULL if insufficient memory
  */
 char *_strdup(char *str)
 {
-	char *ptr = NULL;
-	int len = 0;
+    char *ptr = NULL;
+    int len = 0;
+    char *str_copy = str;
 
-	if (!str)
-	{
-		return (NULL);
-	}
+    if (!str)
+    {
+        return (NULL);
+    }
 
-	while (*str)
-	{
-		len++;
-		str++;
-	}
-	
-	ptr = (char *) malloc(sizeof(char) * (len + 1));
-	
-	if (ptr)
-	{
-		while (len >= 0)
-		{
-			*(ptr + len--) = *(str--);
-		}
-	}
+    while (*str_copy)
+    {
+        len++;
+        str_copy++;
+    }
 
-	return (ptr);
+    ptr = (char *)malloc(sizeof(char) * (len + 1));
+
+    if (ptr)
+    {
+        int i;
+        for (i = 0; i < len; i++)
+        {
+            ptr[i] = str[i];
+        }
+        ptr[len] = '\0';
+    }
+
+    return ptr;
 }
