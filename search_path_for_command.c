13475c13427a222d6b82b80c850c8572f6086fb7
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-20 23:42:04 +0100

    fix

--- search_path_for_command.c
+++ search_path_for_command.c
@@ -24,14 +24,39 @@ char *search_path_for_command(char *path, param_t *params)
 
         if (access(exeArg, F_OK) == 0)
         {
-            return exeArg;
+            return (exeArg);
         }
+
+        write(STDERR_FILENO, "not found\n", strlen("not found\n"));
 
         free(exePath);
         exePath = token_(path, ":", &state);
     }
 
     params->status = 127;
-    write_error_message(params, "not found\n");
-    return NULL;
+    return (NULL);
+}
+
+/**
+ * check_current_directory - Check if the command is executable in the current directory.
+ * @params: The parameters.
+ *
+ * Return: A string of the valid path to the command file, or NULL if not found.
+ */
+char *check_current_directory(param_t *params)
+{
+    char *command_path = params->args[0];
+
+    if (access(command_path, F_OK | X_OK) == 0)
+    {
+        return (str_duplicate(command_path));
+    }
+
+    if (access(command_path, F_OK) == 0)
+    {
+        params->status = 126;
+        write(STDERR_FILENO, "Permission denied\n", strlen("Permission denied\n"));
+    }
+
+    return (NULL);
 }
--- write_error_message.c
+++ write_error_message.c
@@ -9,27 +9,3 @@ void write_error_message(const char *message)
 {
     write(STDERR_FILENO, message, strlen(message));
 }
-
-/**
- * check_current_directory - Check if the command is executable in the current directory.
- * @params: The parameters.
- *
- * Return: A string of the valid path to the command file, or NULL if not found.
- */
-char *check_current_directory(param_t *params)
-{
-    char *command_path = params->args[0];
-
-    if (access(command_path, F_OK | X_OK) == 0)
-    {
-        return (str_duplicate(command_path));
-    }
-
-    if (access(command_path, F_OK) == 0)
-    {
-        params->status = 126;
-        write_error_message(params, "Permission denied\n");
-    }
-
-    return (NULL);
-}
