d55b4eac436c8291bfda1c3c606d7256362515cd
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-20 23:18:13 +0100

    fix

--- run_command.c
+++ run_command.c
@@ -1,40 +1,51 @@
 #include "shell.h"
 
 /**
- * run_command - searches path directories for command and executes it
+ * run_command - searches path directories for a command and executes it
  * @params: parameters
  */
 void run_command(param_t *params)
 {
-	char *exeFile = NULL;
-	pid_t pid;
-	void (*buildin)(param_t *);
+    char *exeFile = NULL;
+    pid_t pid;
 
-	buildin = get_builtin(params);
-	if (buildin)
-	{
-		buildin(params);
-		return;
-	}
-	exeFile = write_error_message(params);
-	if (!exeFile)
-	{
-		return;
-	}
-	pid = fork();
-	if (pid < 0)
-	{
-		free_params(params);
-		exit(98);
-	}
-	else if (pid == 0)
-	{
-		execve(exeFile, params->args, NULL);
-	}
-	else
-	{
-		wait(&params->status);
-		params->status = WEXITSTATUS(params->status);
-		free(exeFile);
-	}
+    void (*buildin)(param_t *);
+
+    buildin = get_builtin(params);
+
+    if (buildin)
+    {
+        buildin(params);
+        return;
+    }
+
+    exeFile = search_path_for_command(params);
+    if (!exeFile)
+    {
+        return;
+    }
+
+    pid = fork();
+    if (pid < 0)
+    {
+        free_params(params);
+        free(exeFile);
+        exit(98);
+    }
+    else if (pid == 0)
+    {
+        if (execve(exeFile, params->args, params->env) == -1)
+        {
+            perror("execve"); // Print an error message if execve fails
+            free_params(params);
+            free(exeFile);
+            exit(127);
+        }
+    }
+    else
+    {
+        wait(&params->status);
+        params->status = WEXITSTATUS(params->status);
+        free(exeFile);
+    }
 }
