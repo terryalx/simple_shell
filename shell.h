82046d123d9bc3a0694c6d7e1e552c0d68da6280
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-20 23:32:23 +0100

    fix

--- shell.h
+++ shell.h
@@ -67,7 +67,7 @@ char *string_copy(char *dest, const char *src);
 char **array_realloc(char **ptr, unsigned int old_size, unsigned int new_size);
 char *token_(char *str, char *delim, char **savePtr);
 char *get_env_value(char *name, param_t *params);
-void write_error_message(param_t *params, const char *message);
+void write_error_message(const char *message);
 char *search_path_for_command(char *path, param_t *params);
 /**/
 void run_command(param_t *);
