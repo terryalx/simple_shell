e25879d196f52dda78a4d1b701b2123f71d80fa1
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-20 12:49:28 +0100

    **realloc

--- _realloc.c
+++ _realloc.c
@@ -1,42 +1,39 @@
 #include "shell.h"
 
 /**
- * _realloc - Reallocates a memory block using malloc.
- * @ptr: Pointer to the old block.
- * @old_size: Size of the old block.
- * @new_size: Size of the new block.
+ * _realloc - Reallocates an array of pointers with memory copying.
+ * @ptr: Pointer to the old array.
+ * @old_size: Size of the old array.
+ * @new_size: Size of the new array.
  *
- * Return: Pointer to the new block.
+ * Return: Pointer to the new array.
  */
-char **_realloc(char **ptr, unsigned int old_size, unsigned int new_size)
+char  **_realloc(char **ptr, unsigned int old_size, unsigned int new_size)
 {
-	char **new_ptr = NULL;
-	unsigned int min_size;
-	unsigned int i;
+    char **new_ptr = NULL;
 
-	if (!ptr)
-	{
-		free(ptr);
-		return (malloc(sizeof(*ptr) * new_size));
-	}
-	else if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
-	min_size = old_size < new_size ? old_size : new_size;
-	new_ptr = malloc(sizeof(*new_ptr) * new_size);
-	if (new_ptr)
-	{
-		for (i = 0; i < new_size; i++)
-			new_ptr[i] = NULL;
-		for (i = 0; i < min_size; i++)
-			new_ptr[i] = ptr[i];
-		free(ptr);
-	}
-	return (new_ptr);
+    if (!ptr)
+        return malloc(sizeof(*ptr) * new_size);
+
+    if (new_size == 0)
+    {
+        free(ptr);
+        return NULL;
+    }
+
+    if (new_size == old_size)
+        return ptr;
+
+    new_ptr = malloc(sizeof(*new_ptr) * new_size);
+    if (!new_ptr)
+        return NULL;
+
+    for (unsigned int i = 0; i < new_size; i++)
+        new_ptr[i] = NULL;
+
+    for (unsigned int i = 0; i < old_size && i < new_size; i++)
+        new_ptr[i] = ptr[i];
+
+    free(ptr);
+    return new_ptr;
 }
