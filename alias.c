7778fc4215f5f0fc89ece6fb0664f1f7d479c949
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-21 18:30:05 +0100

    __update__

--- alias.c
+++ alias.c
@@ -1,10 +1,15 @@
 #include "shell.h"
 
 /**
- * _alias - "alias" shell command.
- * @params: Pointer -> param_t struct.
+ * _alias - Implements the "alias" shell command.
+ * @params: Pointer to param_t struct.
+ *
+ * This function handles the "alias" command in the shell. It allows the user to
+ * create and manage aliases for commands. If no arguments are provided, it
+ * prints all the aliases currently defined. If an argument is provided in the
+ * form of "alias_name=command", it sets the alias. If an argument is provided
+ * without the "=" sign, it prints the command associated with the alias.
  */
-
 void _alias(param_t *params)
 {
 	unsigned int i;
@@ -14,9 +19,10 @@ void _alias(param_t *params)
 		print_all_aliases(params);
 		return;
 	}
+
 	for (i = 1; i < params->tokCount; i++)
 	{
-		if (find_character_in_string(params->args[i], '='))
+		if (_strchr(params->args[i], '='))
 		{
 			set_alias(params->args[i], params);
 		}
