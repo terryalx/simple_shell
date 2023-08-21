8c196a8d73b0d4165572e11152b193a218f7c8ce
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-21 18:14:34 +0100

    __update__

--- str_length.c
+++ str_length.c
@@ -1,22 +1,37 @@
 #include "shell.h"
 
 /**
- * str_length - Get the length of a string.
- *
- * This function calculates the length of a string, excluding the null terminator.
- *
+ * str_length - This function calculates the length of a string, excluding the null terminator.
  * @str: The input string.
  *
  * Return: The length of the string.
  */
 void str_length(param_t *params)
 {
-    size_t length = 0;
+	char *name = params->args[1];
+	list_t *prev = NULL, *h = params->env_head;
 
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-
-    return length;
+	if (params->tokCount != 2)
+	{
+		params->status = 0;
+		return;
+	}
+	while (h)
+	{
+		if (_strcmp(name, h->str) == 0)
+		{
+			if (h == params->env_head)
+				params->env_head = h->next;
+			else
+				prev->next = h->next;
+			free(h->str);
+			free(h->val);
+			free(h);
+			params->status = 0;
+			return;
+		}
+		prev = h;
+		h = h->next;
+	}
+	params->status = 0;
 }
