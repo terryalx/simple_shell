6464764ea298cee630ed28584e0ac8801dc4a421
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-21 17:13:46 +0100

    __update__

--- shell.h
+++ shell.h
@@ -131,7 +131,7 @@ int isDelim(char c, char *delim);
 int string_to_int(char *s); /* _atoi */
 int my_printf(const char *, ...); /* _printf */
 int string_compare(char *, char *); /* _strcmp */
-int strncmp_custom(char *str1, char *str2, int n) /* _strcmp_n */
+int strncmp_custom(char *str1, char *str2, int n); /* _strcmp_n */
 void set_or_update_environment_variable(param_t *params); /* _setenv */
 char *str_duplicate(char *); /* _strdup */
 char *find_character_in_string(char *s, char c); /* _strchr */
