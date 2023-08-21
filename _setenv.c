f15d4040561d8147acdc9853b03c1203da747dd3
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-21 16:30:28 +0100

    __update__

--- _setenv.c
+++ _setenv.c
@@ -1,45 +1,47 @@
 #include "shell.h"
 
 /**
- * _setenv - function searches list for environment variable name.
- * @params: parameters
+ * _setenv - Set or update an environment variable.
+ * @params: Parameters struct.
+ *
+ * This function sets or updates an environment variable.
  *
  * Return: void
  */
 void _setenv(param_t *params)
 {
-	int val = 0;
-	char *tmp = NULL;
-	char *name = params->args[1];
-	char *value = params->args[2];
+    int val = 0;
+    char *tmp = NULL;
+    char *name = params->args[1];
+    char *value = params->args[2];
 
-	list_t *h = params->env_head;
+    list_t *h = params->env_head;
 
-	if (params->tokCount != 3)
-	{
-		params->status = 0;
-		return;
-	}
+    if (params->tokCount != 3)
+    {
+        params->status = 0;
+        return;
+    }
 
-	while (h)
-	{
-		if (string_compare(name, h->str) == 0)
-		{
-			tmp = h->value;
-			free(tmp);
-			h->value = str_duplicate(value);
-			if (!h->value)
-			{
-				write(STDERR_FILENO, "setenv malloc error\n", 18);
-				exit(val);
-			}
-			h->value_length = _strlen(value);
-			params->status = 0;
-			return;
-		}
-		h = h->next_node;
-	}
+    while (h)
+    {
+        if (string_compare(name, h->str) == 0)
+        {
+            tmp = h->value;
+            free(tmp);
+            h->value = str_duplicate(value);
+            if (!h->value)
+            {
+                write(STDERR_FILENO, "Failed to allocate memory for environment variable: %s\n", name);
+                exit(val);
+            }
+            h->value_length = _strlen(value);
+            params->status = 0;
+            return;
+        }
+        h = h->next_node;
+    }
 
-	params->env_head = add_node(&(params->env_head), name, value);
-	params->status = 0;
+    params->env_head = add_node(&(params->env_head), name, value);
+    params->status = 0;
 }
