e4cef3c382112f7611594650c468d1ea3210f327
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-21 18:24:05 +0100

    __add new node__

--- add_node.c
+++ add_node.c
@@ -1,26 +1,34 @@
 #include "shell.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
- * @head: start of list
- * @str: key
- * @val: value
- * Return: ptr to new head
+ * add_node - Add a new node at the beginning of a list.
+ *
+ * This function allocates memory for a new node and adds it to the beginning
+ * of a linked list. It stores the provided string and value in the new node.
+ *
+ * @head: Pointer to the head of the list.
+ * @str: The string to be stored in the new node.
+ * @val: The value to be stored in the new node.
+ *
+ * Return: Pointer to the new head of the list (the newly added node).
  */
-
 list_t *add_node(list_t **head, char *str, char *val)
 {
-	list_t *new;
+    list_t *new_node;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = str_duplicate(str);
-	new->length = _strlen(new->str);
-	new->value = str_duplicate(val);
-	new->value_length = _strlen(val);
-	new->next_node = *head;
-	*head = new;
+    new_node = malloc(sizeof(list_t));
 
-	return (new);
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
+
+    new_node->str = str_duplicate(str);
+    new_node->length = _strlen(new_node->str);
+    new_node->value = str_duplicate(val);
+    new_node->value_length = _strlen(val);
+    new_node->next_node = *head;
+    *head = new_node;
+
+    return (new_node);
 }
