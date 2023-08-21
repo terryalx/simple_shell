63779297bc7b872c2d1bac77ee0ac608e8ccc7ea
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-21 04:51:47 +0100

    _printf

--- _printf.c
+++ _printf.c
@@ -1,15 +1,16 @@
 #include "shell.h"
+
 #define BUFFER_SIZE 1024
 
 /**
- * _printf - output text to standard output specified by format
- * @format: directives for outputing text
- * Return: number of characters output
+ * _printf - Format and print data to the standard output.
+ * @format: A format string specifying the format of the output.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int formatIndex, charCount = 0, returnValue = -1, bufferIndex = 0;
-	char *arg = NULL;
+	int charCount = 0, returnValue = -1, bufferIndex = 0;
 	char buffer[BUFFER_SIZE] = {0};
 	va_list params;
 
@@ -21,83 +22,146 @@ int _printf(const char *format, ...)
 		return (returnValue);
 	}
 
-	formatIndex = 0;
 	va_start(params, format);
+	charCount = process_format(format, params, buffer, &bufferIndex);
+	va_end(params);
+
+	return (charCount);
+}
+
+/**
+ * process_format - Process the format string and print to buffer.
+ * @format: The format string.
+ * @params: The variable argument list.
+ * @buffer: The buffer to store the formatted output.
+ * @bufferIndex: The current index in the buffer.
+ *
+ * Return: The number of characters printed.
+ */
+int process_format(const char *format, va_list params, char *buffer, int *bufferIndex)
+{
+	int formatIndex = 0, charCount = 0;
+	char *arg = NULL;
 
 	while (1)
 	{
-		if (bufferIndex == BUFFER_SIZE)
+		if (*bufferIndex == BUFFER_SIZE)
 		{
-			charCount += flush_buffer(buffer, &bufferIndex);
+			charCount += flush_buffer(buffer, bufferIndex);
 		}
 
 		if (format[formatIndex] == '%')
 		{
-			get_type((char *)format, &formatIndex);
-
-			switch (format[formatIndex])
-			{
-				case 'c':
-					buffer[bufferIndex] = (char) va_arg(params, int);
-					bufferIndex++;
-					formatIndex++;
-					continue;
-				case 's':
-					arg = get_arg('s', va_arg(params, char*));
-					break;
-				case 'd':
-				case 'i':
-					arg = get_arg('d', va_arg(params, int));
-					break;
-				case 'b':
-					arg = get_arg('b', va_arg(params, int));
-					break;
-				case 'r':
-					arg = get_arg('r', va_arg(params, char *));
-					break;
-				case 'R':
-					arg = get_arg('R', va_arg(params, char *));
-					break;
-				case '%':
-					arg = malloc(2);
-					arg[0] = '%';
-					arg[1] = '\0';
-					break;
-				case '\0':
-					buffer[bufferIndex] = '%';
-					bufferIndex++;
-					continue;
-				default:
-					arg = malloc(3);
-					arg[0] = '%';
-					arg[1] = format[formatIndex];
-					arg[2] = '\0';
-			}
-
-			if (!arg)
-			{
-				va_end(params);
-				free(arg);
-				return (returnValue);
-			}
-
-			charCount += flush_buffer(buffer, &bufferIndex);
-			charCount += print_arg(arg);
-			free(arg);
-			formatIndex++;
+			formatIndex = handle_format(format, params, buffer, bufferIndex, &charCount, formatIndex);
 		}
 		else if (format[formatIndex] != '\0')
 		{
-			buffer[bufferIndex] = format[formatIndex];
-			bufferIndex++;
+			buffer[*bufferIndex] = format[formatIndex];
+			(*bufferIndex)++;
 			formatIndex++;
 		}
 		else
 		{
-			charCount += flush_buffer(buffer, &bufferIndex);
-			va_end(params);
+			charCount += flush_buffer(buffer, bufferIndex);
 			return (charCount);
 		}
+	}
+
+	return (charCount);
+}
+
+/**
+ * handle_format - Handle a format specifier.
+ * @format: The format string.
+ * @params: The variable argument list.
+ * @buffer: The buffer to store the formatted output.
+ * @bufferIndex: The current index in the buffer.
+ * @charCount: The total character count.
+ * @formatIndex: The current index in the format string.
+ *
+ * Return: The updated formatIndex.
+ */
+int handle_format(const char *format, va_list params, char *buffer, int *bufferIndex, int *charCount, int formatIndex)
+{
+	char *arg = NULL;
+
+	formatIndex++;
+	if (format[formatIndex] == '\0')
+	{
+		buffer[*bufferIndex] = '%';
+		(*bufferIndex)++;
+		return (formatIndex);
+	}
+
+	switch (format[formatIndex])
+	{
+		case 'c':
+			buffer[*bufferIndex] = (char)va_arg(params, int);
+			(*bufferIndex)++;
+			formatIndex++;
+			break;
+		case 's':
+		case 'd':
+		case 'i':
+		case 'b':
+		case 'r':
+		case 'R':
+			arg = get_arg(format[formatIndex], va_arg(params, void *));
+			*charCount += flush_buffer(buffer, bufferIndex);
+			*charCount += print_arg(arg);
+			free(arg);
+			formatIndex++;
+			break;
+		case '%':
+			buffer[*bufferIndex] = '%';
+			(*bufferIndex)++;
+			formatIndex++;
+			break;
+		default:
+			arg = get_arg(format[formatIndex], NULL);
+			*charCount += flush_buffer(buffer, bufferIndex);
+			*charCount += print_arg(arg);
+			free(arg);
+			formatIndex++;
+	}
+
+	return (formatIndex);
+}
+
+/**
+ * flush_buffer - Write buffer content to stdout.
+ * @buffer: The buffer to be flushed.
+ * @bufferIndex: The current index in the buffer.
+ *
+ * Return: The number of characters flushed.
+ */
+int flush_buffer(char *buffer, int *bufferIndex)
+{
+	int charCount = 0;
+
+	if (*bufferIndex > 0)
+	{
+		charCount = write(1, buffer, *bufferIndex);
+		*bufferIndex = 0;
+	}
+
+	return (charCount);
+}
+
+/**
+ * print_arg - Print the argument string to buffer.
+ * @arg: The argument string.
+ *
+ * Return: The number of characters printed.
+ */
+int print_arg(char *arg)
+{
+	int charCount = 0;
+
+	if (arg)
+	{
+		charCount = _strlen(arg);
+		write(1, arg, charCount);
 	}
 
 	return (charCount);
