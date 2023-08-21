078b8aa0ee53bc76b5559ca893f1b1d065de02f6
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-21 01:45:30 +0100

    echo error

--- strcpy.c
+++ strcpy.c
@@ -11,13 +11,12 @@ char *strcpy(char *dest, const char *src)
 {
     int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-
-    dest[i] = '\0';
-
-    return dest;
+   while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (src[i] == '\n')
+		dest[i] = '\n';
+	return (dest);
 }
