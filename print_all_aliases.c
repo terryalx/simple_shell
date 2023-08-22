ad4da0924c4391ab4f5e7ab21dc1997aca20ea48
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-22 06:27:23 +0100

    __up_date__

--- print_all_aliases.c
+++ print_all_aliases.c
@@ -1,12 +1,14 @@
 #include "shell.h"
 
 /**
- * print_all_aliases - prints all aliases in the alias list
- * @params: alias list
-*/
-
+ * print_all_aliases - Prints all aliases in the alias list.
+ * @params: Parameter struct containing the alias list.
+ *
+ * This function iterates through the alias list stored in the parameter struct
+ * and prints the name and value of each alias to stdout.
+ */
 void print_all_aliases(param_t *params)
 {
-	print_list_alias(params->alias_head);
-	params->status = 0;
+    print_list_alias(params->alias_head);
+    params->status = 0;
 }
--- print_list_alias.c
+++ print_list_alias.c
@@ -1,17 +1,22 @@
 #include "shell.h"
 
 /**
- * print_list_alias - function that prints the name
- * and value of each alias in a linked list.
- * @head: pointer to the head of the linked list.
-*/
-
-void print_list_alias(list_t *head)
+ * print_alias_list - Prints the name and value of each alias in a linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * This function recursively traverses a linked list of aliases, printing the
+ * name and value of each alias to stdout. It starts from the beginning of
+ * the list and works its way to the end.
+ */
+void print_alias_list(list_t *head)
 {
-	if (head)
-	{
-		print_list_alias(head->next_node);
-		if (head->str != NULL)
-			my_printf("%s=\'%s\'\n", head->str, head->value);
-	}
+    if (head == NULL)
+        return;
+
+    print_alias_list(head->next_node);
+
+    if (head->str != NULL)
+    {
+        my_printf("%s='%s'\n", head->str, head->value);
+    }
 }
