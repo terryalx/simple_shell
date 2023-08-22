16b1df5e13513fcd1713feb36847f69e494b962a
Lukacham <danboyiluka2@gmail.com>
2023-08-22 13:03:56 -0700

    _header_

--- shell.h
+++ shell.h
@@ -123,7 +123,7 @@ int strncmp_custom(char *str1, char *str2, int n);
 void set_or_update_environment_variable(param_t *params);
 char *str_duplicate(char *);
 char *find_character_in_string(char *s, char c);
-char **array_realloc(char **ptr, unsigned int old_size, unsigned int new_size); /* _realloc.c */
+char **array_realloc(char **ptr, unsigned int old_size, unsigned int new_size);
 char *string_copy(char *dest, const char *src); /* _strcpy */
 char *token_(char *str, char *delim, char **savePtr); /* _strtok */
 void remove_environment_variable(param_t *params); /* _unsetenv | str_length */
