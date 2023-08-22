ed79699e6395f3b2e1c8c498fa8a4948e87b68a0
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-22 15:32:36 +0100

    __update__

--- string_nconcat.c
+++ string_nconcat.c
@@ -7,7 +7,6 @@
  * @n: amount of bytes for s2
  * Return: Null on failure, else ptr to malloc
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
@@ -15,38 +14,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int j = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
 	while (s2[j] != '\0')
-	{
 		j++;
-	}
+
 	if (n > j)
-	{
 		n = j;
-	}
+
 	ptr = malloc(sizeof(char) * (n + i) + 1);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		ptr[i] = s1[i];
-	}
+
 	for (j = 0; j < n; j++)
-	{
 		ptr[i + j] = s2[j];
-	}
+
 	ptr[i + j] = '\0';
 	return (ptr);
 }
