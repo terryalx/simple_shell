56b3876e406f08eaaad1028541676327093224d5
Lukacham <danboyiluka2@gmail.com>
2023-08-22 13:07:57 -0700

    _header_

--- shell.h
+++ shell.h
@@ -134,7 +134,7 @@ void (*find_builtin_function(param_t *params))(param_t *);
 void create_alias(char *name, char *value, param_t *params);
 void myExit(param_t *params);
 int read_line(param_t *);
-char *realloc_line_buffer(char **line, unsigned int oldSize, unsigned int newSize); /* rlLine */
+char *realloc_line_buffer(char **line, unsigned int oldSize, unsigned int newSize);
 int write_and_reset_buffer(char *buffer, int *index); /* flush_buffer */
 
 #endif /* end __SHELL__H__ */
