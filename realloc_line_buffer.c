7a4042ca949b2c3e256f25e56dc80c63d839254a
Lukacham <danboyiluka2@gmail.com>
2023-08-22 13:31:45 -0700

    more betty code style

--- realloc_line_buffer.c
+++ realloc_line_buffer.c
@@ -4,14 +4,17 @@
 #define READ_SIZE 1024
 
 /**
- * realloc_line_buffer - Reallocates the line buffer to a new size.
+ * realloc_line_buffer - Reallocates the line buffer
+ * to a new size.
  * @line: Pointer to the line buffer.
  * @oldSize: Current size of the line buffer.
  * @newSize: New size of the line buffer.
  *
- * Return: Pointer to the new allocated buffer, or NULL on failure.
+ * Return: Pointer to the new allocated buffer,
+ * or NULL on failure.
  */
-char *realloc_line_buffer(char **line, unsigned int oldSize, unsigned int newSize)
+char *realloc_line_buffer(char **line, unsigned int oldSize,
+		unsigned int newSize)
 {
 	char *newLine = NULL;
 	unsigned int i;
