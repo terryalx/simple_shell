9029d256454a8e5a079dae265bbf386bc01591fc
Lukacham <danboyiluka2@gmail.com>
2023-08-22 13:35:22 -0700

    lines

--- start_shell_main.c
+++ start_shell_main.c
@@ -8,79 +8,79 @@
  * @argc: argument count
  * @argv: Null terminated argument list
  * @env: Null terminated environment variables list
- * 
+ *
  * Return: 0 on success
  */
 int main(int __attribute__((unused)) argc, char **argv, char **env)
 {
-    param_t *params = NULL;
-    int cond = -2, status;
-    unsigned int i;
-    char *state = NULL;
-    size_t size = BUFFER_SIZE;
+	param_t *params = NULL;
+	int cond = -2, status;
+	unsigned int i;
+	char *state = NULL;
+	size_t size = BUFFER_SIZE;
 
-    params = init_param(argv, env);
-    if (!params)
-        exit(-1);
+	params = init_param(argv, env);
+	if (!params)
+		exit(-1);
 
-    signal(SIGINT, sigint_handler);
+	signal(SIGINT, sigint_handler);
 
-    while (1)
-    {
-        if (cond == -1)
-        {
-            status = params->status;
-            my_printf("$ :\n");
-            free_params(params);
-            return (status);
-        }
+	while (1)
+	{
+		if (cond == -1)
+		{
+			status = params->status;
+			my_printf("$ :\n");
+			free_params(params);
+			return (status);
+		}
 
-        for (i = 0; i < BUFFER_SIZE; i++)
-            (params->buffer)[i] = 0;
+		for (i = 0; i < BUFFER_SIZE; i++)
+			(params->buffer)[i] = 0;
 
-        params->tokCount = 0;
+		params->tokCount = 0;
 
-        if (isatty(STDIN_FILENO))
-            my_printf("$: ");
+		if (isatty(STDIN_FILENO))
+			my_printf("$: ");
 
-        cond = getline(&params->buffer, &size, stdin);
+		cond = getline(&params->buffer, &size, stdin);
 
-        params->lineCount++;
+		params->lineCount++;
 
-        if (cond == -1 && _strlen(params->buffer) == 0)
-        {
-            status = params->status;
-            free_params(params);
-            return (status);
-        }
+		if (cond == -1 && _strlen(params->buffer) == 0)
+		{
+			status = params->status;
+			free_params(params);
+			return (status);
+		}
 
-        state = NULL;
-        params->nextCommand = token_(params->buffer, ";\n", &state);
+		state = NULL;
+		params->nextCommand = token_(params->buffer, ";\n", &state);
 
-        while (params->nextCommand)
-        {
-            params->tokCount = process_string(params);
-            if (params->tokCount == 0)
-                break;
+		while (params->nextCommand)
+		{
+			params->tokCount = process_string(params);
+			if (params->tokCount == 0)
+				break;
 
-            if (strcmp(params->args[0], "exit") == 0 && params->tokCount == 2)
-            {
-                int exit_status = atoi(params->args[1]);
-                free_params(params);
-                exit(exit_status);
-            }
+			if (strcmp(params->args[0], "exit") == 0 && params->tokCount == 2)
+			{
+				int exit_status = atoi(params->args[1]);
+				free_params(params);
+				exit(exit_status);
+			}
 
-            execute_external_command(params);
+			execute_external_command(params);
 
-            for (i = 0; i < params->argsCap; i++)
-            {
-                free(params->args[i]);
-                params->args[i] = NULL;
-            }
+			for (i = 0; i < params->argsCap; i++)
+			{
+				free(params->args[i]);
+				params->args[i] = NULL;
+			}
 
-            params->tokCount = 0;
-            free(params->nextCommand);
-            params->nextCommand = token_(params->buffer, ";\n", &state);
-        }
-    }
+			params->tokCount = 0;
+			free(params->nextCommand);
+			params->nextCommand = token_(params->buffer, ";\n", &state);
+		}
+	}
 }
