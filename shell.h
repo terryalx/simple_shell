94f87d86282dbc03c40e149f51cd11d18fa98bd7
Lukacham <danboyiluka2@gmail.com>
2023-08-22 13:06:12 -0700

    _header_

--- shell.h
+++ shell.h
@@ -129,7 +129,7 @@ char *token_(char *str, char *delim, char **savePtr);
 void remove_environment_variable(param_t *params);
 list_t *prepend_list_node(list_t **head, char *str, char *val);
 void change_directory(param_t *params);
-void clear_terminal_screen(param_t *params); /* _clear */
+void clear_terminal_screen(param_t *params);
 void (*find_builtin_function(param_t *params))(param_t *); /* get_builtin */
 void create_alias(char *name, char *value, param_t *params); /* make_alias */
 void myExit(param_t *params); /* _myExit */
