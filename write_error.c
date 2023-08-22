9d398f9cd6aae8069415a9be97353ca1b1d406c9
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-22 07:20:36 +0100

    __error message__

--- write_error.c
+++ write_error.c
@@ -11,30 +11,16 @@
  */
 void write_error(param_t *params, char *msg)
 {
-    char errBuffer[1024] = {0};  /* Error buffer */
-    char *writeHead = errBuffer;  /* Pointer to the current write position */
-
-    /* Copy program name */
+    char errBuffer[1024] = {0};
+    char *writeHead = errBuffer;
+    
     writeHead = stpncpy(writeHead, params->argv[0], sizeof(errBuffer) - (writeHead - errBuffer));
-
-    /* Copy separator ": " */
     writeHead = stpncpy(writeHead, ": ", sizeof(errBuffer) - (writeHead - errBuffer));
-
-    /* Copy line number */
     writeHead = stpncpy(writeHead, get_number(params->lineCount), sizeof(errBuffer) - (writeHead - errBuffer));
-
-    /* Copy separator ": " */
     writeHead = stpncpy(writeHead, ": ", sizeof(errBuffer) - (writeHead - errBuffer));
-
-    /* Copy argument */
     writeHead = stpncpy(writeHead, params->args[0], sizeof(errBuffer) - (writeHead - errBuffer));
-
-    /* Copy separator ": " */
     writeHead = stpncpy(writeHead, ": ", sizeof(errBuffer) - (writeHead - errBuffer));
-
-    /* Copy error message */
     stpncpy(writeHead, msg, sizeof(errBuffer) - (writeHead - errBuffer));
 
-    /* Write error message to stderr */
     write(STDERR_FILENO, errBuffer, strlen(errBuffer));
 }
