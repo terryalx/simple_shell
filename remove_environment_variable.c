09fa05b26c78289b4aeeead197bf269c076d58fe
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-22 05:39:25 +0100

    __up_date__

--- remove_environment_variable.c
+++ remove_environment_variable.c
@@ -1,40 +1,40 @@
-#include "shell.h"
+#include "shell.current_node"
 
 /**
  * remove_environment_variable - Remove an environment variable from the list.
  * @params: Pointer to the param_t structure.
  *
- * This function removes an environment variable from the list based on the provided name.
+ * This function removes an environment variable from the list based on the provided variable_name.
  * If the variable is found and removed, the status in the param_t structure is set to 0.
  *
  * @params: Pointer to the param_t structure containing the list of environment variables.
  */
 void remove_environment_variable(param_t *params)
 {
-	char *name = params->args[1];
-	list_t *prev = NULL, *h = params->env_head;
+	char *variable_name = params->args[1];
+	list_t *prev_node = NULL, *current_node = params->env_head;
 
 	if (params->tokCount != 2)
 	{
 		params->status = 0;
 		return;
 	}
-	while (h)
+	while (current_node)
 	{
-		if (string_compare(name, h->str) == 0)
+		if (string_compare(variable_name, current_node->str) == 0)
 		{
-			if (h == params->env_head)
-				params->env_head = h->next_node;
+			if (current_node == params->env_head)
+				params->env_head = current_node->next_node;
 			else
-				prev->next_node = h->next_node;
-			free(h->str);
-			free(h->value);
-			free(h);
+				prev_node->next_node = current_node->next_node;
+			free(current_node->str);
+			free(current_node->value);
+			free(current_node);
 			params->status = 0;
 			return;
 		}
-		prev = h;
-		h = h->next_node;
+		prev_node = current_node;
+		current_node = current_node->next_node;
 	}
 	params->status = 0;
 }
