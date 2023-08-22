aedeacbcffd1a3dca15fcf1decb44bbd0f1d5ad9
setalxswe <setalxswe@gmail.com>
2023-08-22 15:06:43 -0700

    alias.c allowed_functions array_realloc.c AUTHORS change_directory.c clear_terminal_screen.c concatenate_strings_with_limit.c create_alias.c execute_external_command.c find_builtin_function.c find_character_in_string.c free_list.c free_params.c get-arg.c get_binary.c get_char.c getenv.c get_file.c get_node.c get_number.c get_rev.c get_rot13.c get_string.c get_type.c hsh init_param.c isDelim.c list_len.c move_left.c myExit.c my_printf.c prepend_list_node.c print_alias.c print_all_aliases.c print_arg.c printenv.c print_list_alias.c print_list.c print_list_env.c process_string.c read_line.c README.md realloc_line_buffer.c remove_environment_variable.c rev_string.c rot13.c runchecker.bash set_alias.c set_or_update_environment_variable.c shell.h sigint_handler.c start_shell_main.c str_concat.c str_duplicate.c string_compare.c string_copy.c string_to_int.c _strlen.c strncmp_custom.c TEST_SUITE token_.c validNum.c write_and_reset_buffer.c __write_error__.c

--- get_file.c
+++ get_file.c
@@ -1,91 +1,97 @@
 #include "shell.h"
 
 /**
- * check_current_directory - check if command exists in the current directory
+ * check_current_directory - check if command
+ * exists in the current directory
  * @params: parameters
- * Return: string of valid path to command file, NULL if not found
+ * Return: string of valid path to command file,
+ * NULL if not found
  * The string needs to be freed
  */
 static char *check_current_directory(param_t *params)
 {
-    char *path = NULL;
+	char *path = NULL;
 
-    if (access(params->args[0], F_OK | X_OK) == 0)
-    {
-        free(path);
-        return (str_duplicate(params->args[0]));
-    }
+	if (access(params->args[0], F_OK | X_OK) == 0)
+	{
+		free(path);
+		return (str_duplicate(params->args[0]));
+	}
 
-    if (errno == EACCES)
-    {
-        params->status = 126;
-        __write_error__(params, "Permission denied\n");
-        return (NULL);
-    }
+	if (errno == EACCES)
+	{
+		params->status = 126;
+		__write_error__(params, "Permission denied\n");
+		return (NULL);
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
 /**
- * search_in_path - search for command in directories listed in PATH
+ * search_in_path - search for command in
+ * directories listed in PATH
  * @params: parameters
- * Return: string of valid path to command file, NULL if not found
+ * Return: string of valid path to command file,
+ * NULL if not found
  * The string needs to be freed
  */
 static char *search_in_path(param_t *params)
 {
-    char *path = _getenv("PATH", params);
-    char *exePath = NULL, *exeArg = NULL, *tmp = NULL;
-    char *state = NULL;
+	char *path = _getenv("PATH", params);
+	char *exePath = NULL, *exeArg = NULL, *tmp = NULL;
+	char *state = NULL;
 
-    if (!path)
-    {
-        __write_error__(params, "not found\n");
-        return (NULL);
-    }
+	if (!path)
+	{
+		__write_error__(params, "not found\n");
+		return (NULL);
+	}
 
-    exePath = token_(path, ":", &state);
-    while (exePath)
-    {
-        tmp = exeArg;
-        exeArg = str_concat(exePath, "/");
-        free(tmp);
-        tmp = exeArg;
-        exeArg = str_concat(exeArg, params->args[0]);
-        free(tmp);
+	exePath = token_(path, ":", &state);
+	while (exePath)
+	{
+		tmp = exeArg;
+		exeArg = str_concat(exePath, "/");
+		free(tmp);
+		tmp = exeArg;
+		exeArg = str_concat(exeArg, params->args[0]);
+		free(tmp);
 
-        if (access(exeArg, F_OK) == 0)
-        {
-            free(path);
-            free(exePath);
-            return (exeArg);
-        }
+		if (access(exeArg, F_OK) == 0)
+		{
+			free(path);
+			free(exePath);
+			return (exeArg);
+		}
 
-        free(exePath);
-        exePath = token_(path, ":", &state);
-    }
+		free(exePath);
+		exePath = token_(path, ":", &state);
+	}
 
-    params->status = 127;
-    __write_error__(params, "not found\n");
-    free(path);
-    free(exePath);
-    free(exeArg);
-    return (NULL);
+	params->status = 127;
+	__write_error__(params, "not found\n");
+	free(path);
+	free(exePath);
+	free(exeArg);
+	return (NULL);
 }
 
 /**
- * get_file - get correct path to a command file
+ * get_file - get correct path to a
+ * command file
  * @params: parameters
- * Return: string of valid path to command file, NULL if not found
+ * Return: string of valid path to
+ * command file, NULL if not found
  * The string needs to be freed
  */
 char *get_file(param_t *params)
 {
-    char *path;
+	char *path;
 
-    path = check_current_directory(params);
-    if (path)
-        return (path);
+	path = check_current_directory(params);
+	if (path)
+		return (path);
 
-    return (search_in_path(params));
+	return (search_in_path(params));
 }
