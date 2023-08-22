81dd6313c2e6288995d7c06e87f047933f6ce323
setalxswe <setalxswe@gmail.com>
2023-08-22 02:26:45 -0700

    alias.c allowed_functions array_realloc.c change_directory.c clear_terminal_screen.c create_alias.c find_builtin_function.c find_character_in_string.c flush_buffer.c free_list.c free_params.c get-arg.c get_binary.c get_char.c getenv.c get_file.c getline.c get_node.c get_number.c get_rev.c get_rot13.c get_string.c get_type.c hsh init_param.c isDelim.c list_len.c main.c move_left.c myExit.c my_printf.c prepend_list_node.c print_alias.c print_all_aliases.c print_arg.c printenv.c print_list_alias.c print_list.c print_list_env.c process_string.c README.md remove_environment_variable.c rev_string.c rlLine.c rot13.c runchecker.bash run_command.c set_alias.c set_or_update_environment_variable.c shell.h sigint_handler.c str_concat.c str_duplicate.c string_compare.c string_copy.c string_nconcat.c string_to_int.c _strlen.c strncmp_custom.c TEST_SUITE token_.c validNum.c __write_error__.c

--- main.c
+++ main.c
@@ -9,58 +9,65 @@
  * @env: Null terminated environment variables list
  * Return: 0 on success
  */
-
 int main(int __attribute__((unused)) argc, char **argv, char **env)
 {
-	param_t *params = NULL;
-	int cond = -2, status;
-	unsigned int i;
-	char *state = NULL;
-	size_t size = BUFFER_SIZE;
+    param_t *params = NULL;
+    int cond = -2, status;
+    unsigned int i;
+    char *state = NULL;
+    size_t size = BUFFER_SIZE;
 
-	params = init_param(argv, env);
-	if (!params)
-		exit(-1);
-	signal(SIGINT, sigint_handler);
-	while (1)
-	{
-		if (cond == -1)
-		{
-			status = params->status;
-			my_printf("$ :\n");
-			free_params(params);
-			return (status);
-		}
-		for (i = 0; i < BUFFER_SIZE; i++)
-			(params->buffer)[i] = 0;
-		params->tokCount = 0;
-		if (isatty(STDIN_FILENO))
-			my_printf("$: ");
-		cond = getline(&params->buffer, &size, stdin);
-		params->lineCount++;
-		if (cond == -1 && _strlen(params->buffer) == 0)
-		{
-			status = params->status;
-			free_params(params);
-			return (status);
-		}
-		state = NULL;
-		params->nextCommand = token_(params->buffer, ";\n", &state);
-		while (params->nextCommand)
-		{
-			params->tokCount = process_string(params);
-			if (params->tokCount == 0)
-				break;
-			run_command(params);
-			for (i = 0; i < params->argsCap; i++)
-			{
-				free(params->args[i]);
-				params->args[i] = NULL;
-			}
-			params->tokCount = 0;
-			free(params->nextCommand);
-			params->nextCommand = token_(params->buffer, ";\n",
-						      &state);
-		}
-	}
+    params = init_param(argv, env);
+    if (!params)
+        exit(-1);
+
+    signal(SIGINT, sigint_handler);
+
+    while (1)
+    {
+        if (cond == -1)
+        {
+            status = params->status;
+            _printf("$ :\n");
+            free_params(params);
+            return (status);
+        }
+
+        /* ... Read input and process logic ... */
+
+        if (params->tokCount != 0 && _strcmp(params->args[0], "exit") == 0)
+        {
+            if (params->tokCount > 2)
+            {
+                write_error(params, "exit: too many arguments\n");
+                params->status = 2;
+            }
+            else if (params->tokCount == 1)
+            {
+                free_params(params);
+                exit(0);
+            }
+            else
+            {
+                if (validNum(params->args[1]))
+                {
+                    status = _atoi(params->args[1]);
+                    free_params(params);
+                    exit(status);
+                }
+                else
+                {
+                    write_error(params, "exit: Illegal number: ");
+                    write(STDERR_FILENO, params->args[1], _strlen(params->args[1]));
+                    write(STDERR_FILENO, "\n", 1);
+                    params->status = 2;
+                }
+            }
+        }
+
+        /* ... Continue processing other commands ... */
+    }
+
+    return (0);
 }
+
