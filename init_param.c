5e7496f982a629aa201a064d1103e47884004028
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-23 01:08:02 +0100

    __init__

--- init_param.c
+++ init_param.c
@@ -11,60 +11,100 @@
  */
 static param_t *allocate_param(char **argv, char **env)
 {
-	param_t *params = malloc(sizeof(*params));
-	unsigned int i;
+    param_t *params = malloc(sizeof(*params));
 
-	if (!params)
-		return (NULL);
+    if (!params)
+        return (NULL);
 
-	params->argsCap = 10;
-	params->lineCount = 0;
-	params->tokCount = 0;
-	params->status = 0;
-	params->argv = argv;
-	params->nextCommand = NULL;
+    params->argsCap = 10;
+    params->lineCount = 0;
+    params->tokCount = 0;
+    params->status = 0;
+    params->argv = argv;
+    params->nextCommand = NULL;
 
-	params->buffer = malloc(sizeof(char) * BUFFER_SIZE);
-	if (!(params->buffer))
-	{
-		free(params);
-		exit(-1);
-	}
-	for (i = 0; i < BUFFER_SIZE; i++)
-		params->buffer[i] = 0;
+    if (!initialize_buffer(params))
+        return (NULL);
 
-	params->args = malloc(sizeof(char *) * params->argsCap);
-	if (!(params->args))
-	{
-		free(params->buffer);
-		free(params);
-		exit(-1);
-	}
-	for (i = 0; i < params->argsCap; i++)
-		params->args[i] = NULL;
+    if (!initialize_args(params))
+        return (NULL);
 
-	params->env_head = NULL;
-	for (i = 0; env[i]; i++)
-	{
-		char *eqs = find_character_in_string(env[i], '=');
-		*eqs = '\0';
+    if (!initialize_environment(params, env))
+        return (NULL);
 
-		params->env_head = prepend_list_node(&(params->env_head),
-				env[i], eqs + 1);
+    params->alias_head = NULL;
 
-		if (!(params->env_head))
-		{
-			free(params->buffer);
-			free(params->args);
-			free_list(params->env_head);
-			free(params);
-			exit(-1);
-		}
-	}
+    return (params);
+}
 
-	params->alias_head = NULL;
+/**
+ * initialize_buffer - Initialize the buffer in the param_t structure.
+ * @params: Pointer to the param_t structure.
+ * Return: 1 on success, 0 on failure.
+ */
+static int initialize_buffer(param_t *params)
+{
+    params->buffer = malloc(sizeof(char) * BUFFER_SIZE);
+    if (!(params->buffer))
+    {
+        free(params);
+        exit(-1);
+    }
+    for (unsigned int i = 0; i < BUFFER_SIZE; i++)
+        params->buffer[i] = 0;
 
-	return (params);
+    return (1);
+}
+
+/**
+ * initialize_args - Initialize the args array in the param_t structure.
+ * @params: Pointer to the param_t structure.
+ * Return: 1 on success, 0 on failure.
+ */
+static int initialize_args(param_t *params)
+{
+    params->args = malloc(sizeof(char *) * params->argsCap);
+    if (!(params->args))
+    {
+        free(params->buffer);
+        free(params);
+        exit(-1);
+    }
+    for (unsigned int i = 0; i < params->argsCap; i++)
+        params->args[i] = NULL;
+
+    return (1);
+}
+
+/**
+ * initialize_environment - Populate environment variables in param_t structure.
+ * @params: Pointer to the param_t structure.
+ * @env: Environment variables.
+ * Return: 1 on success, 0 on failure.
+ */
+static int initialize_environment(param_t *params, char **env)
+{
+    params->env_head = NULL;
+
+    for (unsigned int i = 0; env[i]; i++)
+    {
+        char *eqs = find_character_in_string(env[i], '=');
+        *eqs = '\0';
+
+        params->env_head = prepend_list_node(&(params->env_head),
+                                             env[i], eqs + 1);
+
+        if (!(params->env_head))
+        {
+            free(params->buffer);
+            free(params->args);
+            free_list(params->env_head);
+            free(params);
+            exit(-1);
+        }
+    }
+
+    return (1);
 }
 
 /**
@@ -75,5 +115,5 @@ static param_t *allocate_param(char **argv, char **env)
  */
 param_t *init_param(char **argv, char **env)
 {
-	return (allocate_param(argv, env));
+    return (allocate_param(argv, env));
 }
