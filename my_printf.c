982081c83c464f473d6251898655b5e5dbe4926d
EFE _CEO ALX <setalxswe@gmail.com>
2023-08-23 03:05:03 +0100

    __printf__

--- my_printf.c
+++ my_printf.c
@@ -2,17 +2,11 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * handle_char_format - Handle character format specifier ('c')
- */
 int handle_char_format(char *buffer, int *bufferIndex, va_list params) {
     buffer[(*bufferIndex)++] = (char)va_arg(params, int);
     return 1;
 }
 
-/**
- * handle_string_format - Handle string format specifier ('s')
- */
 int handle_string_format(char *buffer, int *bufferIndex, va_list params) {
     char *arg = va_arg(params, char*);
     int charCount = 0;
@@ -32,12 +26,6 @@ int handle_string_format(char *buffer, int *bufferIndex, va_list params) {
     return charCount;
 }
 
-/**
- * my_printf - Output text to standard output specified by format
- * @format: Directives for outputting text
- *
- * Return: Number of characters output
- */
 int my_printf(const char *format, ...) {
     int formatIndex, charCount = 0, returnValue = -1, bufferIndex = 0;
     char buffer[BUFFER_SIZE] = {0};
@@ -68,6 +56,9 @@ int my_printf(const char *format, ...) {
                     charCount += handle_string_format(buffer, &bufferIndex, params);
                     break;
                 default:
+                    fprintf(stderr, "Error: Unsupported format specifier: %%%c\n", format[formatIndex]);
+                    va_end(params);
+                    return -1;
             }
 
             formatIndex++;
@@ -80,5 +71,6 @@ int my_printf(const char *format, ...) {
             return (charCount);
         }
     }
+
     return (charCount);
 }
